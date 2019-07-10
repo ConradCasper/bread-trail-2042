ac06de432e22910170932a195383c44162e4e2a3
github username <github email address>
2019-07-10 10:01:15 -0400

    login and sign up working properly

--- app/controllers/survivors_controller.rb
+++ app/controllers/survivors_controller.rb
@@ -3,5 +3,35 @@ class SurvivorsController < ApplicationController
     def show
         @survivor = Survivor.find(params[:id])
     end
+
+    def new
+        @survivor = Survivor.new
+    end
+
+    def create
+        @survivor = Survivor.new(survivor_params)
+
+        if @survivor.save 
+            redirect_to login_path
+        else
+            render :new
+        end
+
+    end
+
+
+
+
+
+
+
+    private
+
+
+    def survivor_params
+        params.require(:survivor).permit(:name, :username, :grain, :password, :image_url)
+    end
+
+        
     
 end
