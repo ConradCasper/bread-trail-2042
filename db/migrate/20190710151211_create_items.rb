6df8f86e39998dce1281e1058348a7f5e67181c2
Blake Stoner <stonerblake@gmail.com>
2019-07-10 14:13:09 -0400

    + seeds

--- db/migrate/20190710151211_create_items.rb
+++ db/migrate/20190710151211_create_items.rb
@@ -2,6 +2,9 @@ class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
       t.references :store, foreign_key: true
+      t.string :name
+      t.integer :cost
+      t.string :location
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Survivor.destroy_all 
+Store.destroy_all 
+Item.destroy_all 
+Order.destroy_all 
+OrderItem.destroy_all 
 
 rog = Survivor.create(
         name: "rog",
@@ -47,4 +51,91 @@ adrian = Survivor.create(
     grain: 10000,
     image_url: 'survivors/04_survivor.jpg'
 )
+#################
 
+store = Store.create(
+)
+
+################
+
+order = Order.create(
+survivor_id: adrian.id, 
+store_id: store.id
+)
+
+#################
+
+bread_loaf = Item.create(
+    name: "white bread",
+    cost: 20,
+    store_id: store.id,
+    location: "Majestic's Dinner on Ponce"
+)
+
+first_aid = Item.create(
+    name: "first-aid kit",
+    cost: 200,
+    store_id: store.id,
+    location: "Grady Hospital"
+)
+
+machete = Item.create(
+    name: "machete",
+    cost: 60,
+    store_id: store.id,
+    location: "Home Depot on Ponce"
+)
+
+ham_sandwich = Item.create(
+    name: "Ham Sandwich",
+    cost: 30,
+    store_id: store.id,
+    location: "Publix off Piedmont"
+)
+
+penicillin = Item.create(
+    name: "penicillin",
+    cost: 100,
+    store_id: store.id,
+    location: "CVS"
+)
+
+shotgun = Item.create(
+    name: "shotgun",
+    cost: 80,
+    store_id: store.id,
+    location: "Wal-Mart on Howell Mill"
+)
+
+ beef_jerky = Item.create(
+    name: "beef jerky",
+    cost: 10,
+    store_id: store.id,
+    location: "gas stations"
+ )
+
+beer = Item.create(
+    name: "lager beer",
+    cost: 30,
+    store_id: store.id,
+    location: "Green's on Ponce"
+ )
+
+ #################
+
+ order_item = OrderItem.create(
+    order_id: order.id,
+    item_id: beer.id
+ )
+
+ order_item2 = OrderItem.create(
+    order_id: order.id,
+    item_id: beef_jerky.id
+ )
+
+ order_item3 = OrderItem.create(
+    order_id: order.id,
+    item_id: shotgun.id
+ )
+
+ #################
